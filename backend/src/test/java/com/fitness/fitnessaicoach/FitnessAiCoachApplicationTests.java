e88c7f252bcf33e1f65dd163b629886ca296717d
Jesus Hernandez <jahp88x@gmail.com>
2026-02-25 18:33:50 +0100

    chore: estrcutura inicial del monorepo y backend funcional con ÂpostgreSQL

---
+++ backend/src/test/java/com/fitness/fitnessaicoach/FitnessAiCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.fitnessaicoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAiCoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
